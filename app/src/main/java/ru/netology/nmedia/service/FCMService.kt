77c0dd978516a54abc1e08012a88e3afaaddddd8
Dolgoff62 <dolgov2007@inbox.ru>
2021-03-11 13:29:11 +0300

    Updated push notifications. Now push notifications are sent to a specific (authorized) user.

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -2,24 +2,19 @@ package ru.netology.nmedia.service
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.os.Build
-import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.app.TaskStackBuilder
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import ru.netology.nmedia.R
-import ru.netology.nmedia.activity.AppActivity
+import ru.netology.nmedia.auth.AppAuth
 import kotlin.random.Random
 
 
 class FCMService : FirebaseMessagingService() {
-    private val action = "action"
     private val content = "content"
     private val channelId = "remote"
     private val gson = Gson()
@@ -40,97 +35,26 @@ class FCMService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
 
-        message.data[action]?.let {
-            when (it) {
-                "LIKE" -> handleLike(gson.fromJson(message.data[content], Like::class.java))
-                "SHARE" -> handleShare(gson.fromJson(message.data[content], Share::class.java))
-                "newPost" -> handleNewPost(
-                    gson.fromJson(
-                        message.data[content],
-                        NewPost::class.java
-                    )
-                )
-                else -> handleElse(gson.fromJson(message.data[content], Else::class.java))
-            }
+        val currentRecipient = AppAuth.getInstance().authStateFlow.value.id
+        val content = gson.fromJson(message.data[content], Handler::class.java)
+
+        when (content.recipientId) {
+            currentRecipient, null -> handleForAll(content)
+            else -> AppAuth.getInstance().sendPushToken()
         }
     }
 
     override fun onNewToken(token: String) {
-        println(token)
+        AppAuth.getInstance().sendPushToken(token)
     }
 
-    private fun handleNewPost(content: NewPost) {
-
-        val remoteViews = RemoteViews(packageName, R.layout.notification)
-
-        remoteViews.setTextViewText(
-            R.id.tvNotificationTitle,
-            getString(
-                R.string.notification_new_post,
-                content.author
-            )
-        )
-        remoteViews.setTextViewText(R.id.tvNotification, content.text)
-        val resultIntent = Intent(this, AppActivity::class.java)
-
-        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
-            addNextIntentWithParentStack(resultIntent)
-            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setContentIntent(resultPendingIntent)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setCustomContentView(remoteViews)
-            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(100_000), notification)
-    }
-
-    private fun handleLike(content: Like) {
+    private fun handleForAll(content: Handler) {
         val notification = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.netology)
             .setContentTitle(
                 getString(
-                    R.string.notification_user_liked,
-                    content.userName,
-                    content.postAuthor
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(100_000), notification)
-    }
-
-    private fun handleShare(content: Share) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.netology)
-            .setContentTitle(
-                getString(
-                    R.string.notification_user_share,
-                    content.userName,
-                    content.postAuthor
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-
-        NotificationManagerCompat.from(this)
-            .notify(Random.nextInt(100_000), notification)
-    }
-
-    private fun handleElse(content: Else) {
-        val notification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.netology)
-            .setContentTitle(
-                getString(
-                    R.string.notification_else_action,
-                    content.postAuthor
+                    R.string.notification_for_all,
+                    content.content
                 )
             )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -141,29 +65,7 @@ class FCMService : FirebaseMessagingService() {
     }
 }
 
-data class NewPost(
-    val id: Long,
-    val author: String,
-    val text: String
-)
-
-data class Like(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String
-)
-
-data class Share(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String
-)
-
-data class Else(
-    val userId: Long,
-    val userName: String,
-    val postId: Long,
-    val postAuthor: String
+data class Handler(
+    val recipientId: Long?,
+    val content: String
 )
